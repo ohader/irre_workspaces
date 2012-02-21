c8fffb6bb1212686f75b6f8f570149af802c82ff
Oliver Hader <oliver@typo3.org>
2012-02-21 12:46:32 +0100

    [TASK] Kickstart extension

---
+++ ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "irre_workspaces".
+#
+# Auto generated 21-02-2012 12:42
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'IRRE Workspaces',
+	'description' => 'Special handling for IRRE elements on Workspaces',
+	'category' => 'be',
+	'author' => 'Oliver Hader',
+	'author_email' => 'oliver.hader@typo3.org',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"e14d";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"967f";s:20:"doc/wizard_form.html";s:4:"5e6f";}',
+);
+
+?>
