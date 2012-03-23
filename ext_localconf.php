a9d0b733c620d545f0be99565df559a56f38971c
Oliver Hader <oliver@typo3.org>
2012-03-23 10:53:05 +0100

    [TASK] Add comments for alternatives with TYPO3 4.7

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,8 +12,12 @@ $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-pre
 // Modify the bahaviour of the workspace module
 // Basically the XLCASSes are required to inject behaviour and JavaScript code
 
+// @todo: Might use signal-dispatcher for TYPO3 4.7, see http://forge.typo3.org/issues/35166
+
 $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/workspaces/Classes/Service/GridData.php'] =
 	t3lib_extMgm::extPath($_EXTKEY) . 'Classes/XClasses/Tx_Workspaces_Service_GridData.php';
+
+// @todo: Might use signal-dispatcher for TYPO3 4.7, see http://forge.typo3.org/issues/35175
 
 $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/workspaces/Classes/ExtDirect/ActionHandler.php'] =
 	t3lib_extMgm::extPath($_EXTKEY) . 'Classes/XClasses/Tx_Workspaces_ExtDirect_ActionHandler.php';
@@ -21,6 +25,8 @@ $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/workspaces/Classes/ExtDirect/ActionH
 /*
 DISABLED - Since the workspace module does not show unmodified elements,
 there is no need to manipulate data structures (without changes) on editing
+
+// @todo: Might use processDatamap_beforeStart for TYPO3 4.7, see http://forge.typo3.org/issues/35161
 
 $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_tcemain.php'] =
 	t3lib_extMgm::extPath($_EXTKEY) . 'Classes/XClasses/TceMain.php';
