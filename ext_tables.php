7690a211afaf98759aff82f139f18d1f188a859e
Oliver Hader <oliver@typo3.org>
2012-11-12 09:58:03 +0100

    [TASK] Enable tt_content.pi_flexform field to be rendered in workspace module

--- ext_tables.php
+++ ext_tables.php
@@ -13,6 +13,12 @@ if (TYPO3_MODE == 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
 	);
 }
 
+t3lib_div::loadTCA('tt_content');
+// Enable field pi_flexform to be displayed in workspace record details view:
+if (!t3lib_div::inList($GLOBALS['TCA']['tt_content']['interface']['showRecordFieldList'], 'pi_flexform')) {
+	$GLOBALS['TCA']['tt_content']['interface']['showRecordFieldList'] .= ',pi_flexform';
+}
+
 require_once t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/SysWorkspace.php';
 require_once t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/SysWorkspaceStage.php';
 ?>
