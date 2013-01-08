d692468b5f502a34a696ca3ccb20c6ab594e0618
Oliver Hader <oliver@typo3.org>
2013-01-08 17:08:18 +0100

    [TASK] Refactor action service handlers

--- Classes/Service/Action/AbstractActionService.php
+++ Classes/Service/Action/AbstractActionService.php
@@ -35,12 +35,164 @@ abstract class Tx_IrreWorkspaces_Service_Action_AbstractActionService implements
 	protected $workspacesStagesService;
 
 	/**
+	 * @var array|Tx_IrreWorkspaces_Domain_Model_Dependency_IncompleteStructure[]
+	 */
+	protected $incompleteStructures = array();
+
+	/**
+	 * @var t3lib_TCEmain
+	 */
+	protected $dataHandler;
+
+	/**
+	 * @param t3lib_utility_Dependency_Element $outerMostParent
+	 * @param array|t3lib_utility_Dependency_Element[] $intersectingElements
+	 * @param array|t3lib_utility_Dependency_Element[] $differentElements
+	 * @return Tx_IrreWorkspaces_Domain_Model_Dependency_IncompleteStructure
+	 */
+	public function addIncompleteStructure(t3lib_utility_Dependency_Element $outerMostParent, array $intersectingElements, array $differentElements) {
+		$incompleteStructure = Tx_IrreWorkspaces_Domain_Model_Dependency_IncompleteStructure::create(
+			$outerMostParent,
+			$intersectingElements,
+			$differentElements
+		);
+		$this->incompleteStructures[] = $incompleteStructure;
+		return $incompleteStructure;
+	}
+
+	/**
+	 * @param t3lib_utility_Dependency_Element $element
+	 * @param string $comment
+	 * @return NULL|integer
+	 */
+	protected function cloneLiveVersion(t3lib_utility_Dependency_Element $element, $comment) {
+		$clonedId = $this->getClonedId($element);
+
+		// See, whether a clone has been created already
+		// e.g. during cloning a parent thus all children
+		if (empty($clonedId)) {
+			$versionRecord = t3lib_BEfunc::getWorkspaceVersionOfRecord(
+				$this->getBackendUser()->workspace,
+				$element->getTable(),
+				$this->getLiveId($element),
+				'uid'
+			);
+
+			if (!empty($versionRecord['uid'])) {
+				$clonedId = $versionRecord['uid'];
+				$element->setDataValue('clonedId', $clonedId);
+			}
+		}
+
+		// If there's nothing, clone the live version
+		if (empty($clonedId)) {
+			$clonedId = $this->dataHandler->versionizeRecord(
+				$element->getTable(),
+				$this->getLiveId($element),
+				$comment
+			);
+
+			$element->setDataValue('clonedId', $clonedId);
+		}
+
+		return $clonedId;
+	}
+
+	/**
+	 * @param t3lib_utility_Dependency_Element $element
+	 * @return NULL|integer
+	 */
+	protected function getLiveId(t3lib_utility_Dependency_Element $element) {
+		$liveId = $element->getDataValue('liveId');
+
+		if (empty($liveId)) {
+			$liveId = t3lib_BEfunc::getLiveVersionIdOfRecord($element->getTable(), $element->getId());
+		}
+
+		return $liveId;
+	}
+
+	/**
+	 * @param t3lib_utility_Dependency_Element $element
+	 * @return NULL|integer
+	 */
+	protected function getClonedId(t3lib_utility_Dependency_Element $element) {
+		$clonedId = $element->getDataValue('clonedId');
+		return $clonedId;
+	}
+
+	/**
+	 * @param t3lib_utility_Dependency_Element $element
+	 * @return NULL|integer
+	 */
+	protected function getFallbackId(t3lib_utility_Dependency_Element $element) {
+		$id = $this->getClonedId($element);
+
+		if (empty($id)) {
+			$id = $element->getId();
+		}
+
+		return $id;
+	}
+
+	/**
+	 * @param string $table
+	 * @param integer $id
+	 * @param string $field
+	 * @return NULL|integer
+	 */
+	protected function findRemapStackIndex($table, $id, $field) {
+		$remapStackIndex = NULL;
+
+		if (!empty($this->dataHandler->remapStackRecords[$table])) {
+			foreach ($this->dataHandler->remapStackRecords[$table] as $uniqeId => $data) {
+				$index = $data['remapStackIndex'];
+
+				if ((int) $this->dataHandler->substNEWwithIDs[$uniqeId] === (int) $id) {
+					if ($this->dataHandler->remapStack[$index]['field'] === $field) {
+						$remapStackIndex = $index;
+						break;
+					}
+				}
+			}
+		}
+
+		return $remapStackIndex;
+	}
+
+	/**
+	 * @param integer $remapStackIndex
+	 * @param string $key
+	 * @param mixed $value
+	 */
+	protected function updateRemapStack($remapStackIndex, $key, $value) {
+		if (isset($this->dataHandler->remapStack[$remapStackIndex]['pos'][$key])) {
+			$position = $this->dataHandler->remapStack[$remapStackIndex]['pos'][$key];
+			$this->dataHandler->remapStack[$remapStackIndex]['args'][$position] = $value;
+		}
+	}
+
+	/**
+	 * @param array|t3lib_utility_Dependency_Reference[] $childReferences
+	 * @return array|t3lib_utility_Dependency_Element[]
+	 */
+	public function getChildrenPerParentField(array $childReferences) {
+		$childrenPerParentField = array();
+
+		foreach ($childReferences as $childReference) {
+			$childrenPerParentField[$childReference->getField()][] = $childReference->getElement();
+		}
+
+		return $childrenPerParentField;
+	}
+
+	/**
 	 * @param string $parentTable
 	 * @param integer $parentId
 	 * @param array $parentConfiguration
 	 * @return t3lib_loadDBGroup
 	 */
-	protected function getReferenceCollection($parentTable, $parentId, array $parentConfiguration) {
+	public function getReferenceCollection($parentTable, $parentId, array $parentConfiguration) {
 		/** @var $referenceCollection t3lib_loadDBGroup */
 		$referenceCollection = t3lib_div::makeInstance('t3lib_loadDBGroup');
 		$referenceCollection->start(
@@ -52,6 +204,25 @@ abstract class Tx_IrreWorkspaces_Service_Action_AbstractActionService implements
 			$parentConfiguration
 		);
 		return $referenceCollection;
+	}
+
+	/**
+	 * @param array|t3lib_utility_Dependency_Element[] $elements
+	 * @param string $table
+	 * @param integer $id
+	 * @return NULL|t3lib_utility_Dependency_Element
+	 */
+	public function findElement(array $elements, $table, $id) {
+		$result = NULL;
+
+		foreach ($elements as $element) {
+			if ($element->getTable() === $table && $element->getId() == $id) {
+				$result = $element;
+				break;
+			}
+		}
+
+		return $element;
 	}
 
 	/**
--- Classes/Service/Action/PublishWorkspaceActionService.php
+++ Classes/Service/Action/PublishWorkspaceActionService.php
@@ -30,29 +30,6 @@
  */
 class Tx_IrreWorkspaces_Service_Action_PublishWorkspaceActionService extends Tx_IrreWorkspaces_Service_Action_AbstractActionService {
 	/**
-	 * @var t3lib_TCEmain
-	 */
-	protected $dataHandler;
-
-	/**
-	 * @var array|Tx_IrreWorkspaces_Domain_Model_Dependency_IncompleteStructure[]
-	 */
-	protected $incompleteStructures = array();
-
-	/**
-	 * @param t3lib_utility_Dependency_Element $outerMostParent
-	 * @param array|t3lib_utility_Dependency_Element[] $intersectingElements
-	 * @param array|t3lib_utility_Dependency_Element[] $differentElements
-	 */
-	public function addIncompleteStructure(t3lib_utility_Dependency_Element $outerMostParent, array $intersectingElements, array $differentElements) {
-		$this->incompleteStructures[] = Tx_IrreWorkspaces_Domain_Model_Dependency_IncompleteStructure::create(
-			$outerMostParent,
-			$intersectingElements,
-			$differentElements
-		);
-	}
-
-	/**
 	 * @param t3lib_TCEmain $dataHandler
 	 */
 	public function finish(t3lib_TCEmain $dataHandler) {
@@ -137,16 +114,11 @@ class Tx_IrreWorkspaces_Service_Action_PublishWorkspaceActionService extends Tx_
 	 * @param array|t3lib_utility_Dependency_Reference[] $incompleteChildren
 	 */
 	protected function updateLiveReferences(t3lib_utility_Dependency_Element $parentElement, array $incompleteChildren) {
-		$childrenPerParentField = array();
+		$childrenPerParentField = $this->getChildrenPerParentField($incompleteChildren);
 
 		$parentId = $this->getLiveId($parentElement);
 		$parentTable = $parentElement->getTable();
 		t3lib_div::loadTCA($parentTable);
-
-		/** @var $childReference t3lib_utility_Dependency_Reference */
-		foreach ($incompleteChildren as $childReference) {
-			$childrenPerParentField[$childReference->getField()][] = $childReference->getElement();
-		}
 
 		/** @var $children t3lib_utility_Dependency_Element[] */
 		foreach ($childrenPerParentField as $parentField => $children) {
@@ -183,16 +155,11 @@ class Tx_IrreWorkspaces_Service_Action_PublishWorkspaceActionService extends Tx_
 	 * @param t3lib_utility_Dependency_Element $parentElement
 	 */
 	protected function updateVersionReferences(t3lib_utility_Dependency_Element $parentElement) {
-		$childrenPerParentField = array();
+		$childrenPerParentField = $this->getChildrenPerParentField($parentElement->getChildren());
 
 		$parentId = $this->getFallbackId($parentElement);
 		$parentTable = $parentElement->getTable();
 		t3lib_div::loadTCA($parentTable);
-
-		/** @var $childReference t3lib_utility_Dependency_Reference */
-		foreach ($parentElement->getChildren() as $childReference) {
-			$childrenPerParentField[$childReference->getField()][] = $childReference->getElement();
-		}
 
 		/** @var $children t3lib_utility_Dependency_Element[] */
 		foreach ($childrenPerParentField as $parentField => $children) {
@@ -217,118 +184,6 @@ class Tx_IrreWorkspaces_Service_Action_PublishWorkspaceActionService extends Tx_
 				$this->updateRecord($parentTable, $parentId, array($parentField => count($referenceCollection->itemArray)));
 			}
 		}
-	}
-
-	/**
-	 * @param string $table
-	 * @param integer $id
-	 * @param string $field
-	 * @return NULL|integer
-	 */
-	protected function findRemapStackIndex($table, $id, $field) {
-		$remapStackIndex = NULL;
-
-		if (!empty($this->dataHandler->remapStackRecords[$table])) {
-			foreach ($this->dataHandler->remapStackRecords[$table] as $uniqeId => $data) {
-				$index = $data['remapStackIndex'];
-
-				if ((int) $this->dataHandler->substNEWwithIDs[$uniqeId] === (int) $id) {
-					if ($this->dataHandler->remapStack[$index]['field'] === $field) {
-						$remapStackIndex = $index;
-						break;
-					}
-				}
-			}
-		}
-
-		return $remapStackIndex;
-	}
-
-	/**
-	 * @param integer $remapStackIndex
-	 * @param string $key
-	 * @param mixed $value
-	 */
-	protected function updateRemapStack($remapStackIndex, $key, $value) {
-		if (isset($this->dataHandler->remapStack[$remapStackIndex]['pos'][$key])) {
-			$position = $this->dataHandler->remapStack[$remapStackIndex]['pos'][$key];
-			$this->dataHandler->remapStack[$remapStackIndex]['args'][$position] = $value;
-		}
-	}
-
-	/**
-	 * @param t3lib_utility_Dependency_Element $element
-	 * @return NULL|integer
-	 */
-	protected function getLiveId(t3lib_utility_Dependency_Element $element) {
-		$liveId = $element->getDataValue('liveId');
-
-		if (empty($liveId)) {
-			$liveId = t3lib_BEfunc::getLiveVersionIdOfRecord($element->getTable(), $element->getId());
-		}
-
-		return $liveId;
-	}
-
-	/**
-	 * @param t3lib_utility_Dependency_Element $element
-	 * @return NULL|integer
-	 */
-	protected function getClonedId(t3lib_utility_Dependency_Element $element) {
-		$clonedId = $element->getDataValue('clonedId');
-		return $clonedId;
-	}
-
-	/**
-	 * @param t3lib_utility_Dependency_Element $element
-	 * @return NULL|integer
-	 */
-	protected function getFallbackId(t3lib_utility_Dependency_Element $element) {
-		$id = $this->getClonedId($element);
-
-		if (empty($id)) {
-			$id = $element->getId();
-		}
-
-		return $id;
-	}
-
-	/**
-	 * @param t3lib_utility_Dependency_Element $element
-	 * @param string $comment
-	 * @return NULL|integer
-	 */
-	protected function cloneLiveVersion(t3lib_utility_Dependency_Element $element, $comment) {
-		$clonedId = $this->getClonedId($element);
-
-		// See, whether a clone has been created already
-		// e.g. during cloning a parent thus all children
-		if (empty($clonedId)) {
-			$versionRecord = t3lib_BEfunc::getWorkspaceVersionOfRecord(
-				$this->getBackendUser()->workspace,
-				$element->getTable(),
-				$this->getLiveId($element),
-				'uid'
-			);
-
-			if (!empty($versionRecord['uid'])) {
-				$clonedId = $versionRecord['uid'];
-				$element->setDataValue('clonedId', $clonedId);
-			}
-		}
-
-		// If there's nothing, clone the live version
-		if (empty($clonedId)) {
-			$clonedId = $this->dataHandler->versionizeRecord(
-				$element->getTable(),
-				$this->getLiveId($element),
-				$comment
-			);
-
-			$element->setDataValue('clonedId', $clonedId);
-		}
-
-		return $clonedId;
 	}
 
 	/**
